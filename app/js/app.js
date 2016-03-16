1fa68172f3a899e5b27f7718b2b47b40c9456d3a
railsbeginner <bartosz.dyja@gmail.com>
2016-03-16 17:45:19 +0100

    Add article show page and routes

--- app/js/app.js
+++ app/js/app.js
@@ -3,20 +3,22 @@ var frontendPage = angular.module('frontendPage',['ngRoute', 'ngSanitize'])
 frontendPage.config(['$routeProvider',
   function($routeProvider) {
     $routeProvider.
-      when('/main', {
+      when('/', {
         templateUrl: 'partials/main.html',
         controller: 'FinanceCtrl'
       }).
-      when('/phones/:phoneId', {
-        templateUrl: 'partials/show.html'
+      when('/article/:articleId', {
+        templateUrl: 'partials/article.html',
+        controller: 'ArticleCtrl'
       }).
       otherwise({
-        redirectTo: '/main'
+        redirectTo: '/'
       });
   }]);
 
+
+
   frontendPage.controller('FinanceCtrl', function($scope, $http) {
-    $scope.someMessage = '<b>abs</b> asdfas'
     $http.get('http://www.bdfinanse.pl/bd_site/api/testimonials.php')
          .then(function(res){
             $scope.testimonials = res.data.records;
@@ -26,3 +28,12 @@ frontendPage.config(['$routeProvider',
             $scope.articles = res.data.articles;
           });
   });
+
+
+  frontendPage.controller('ArticleCtrl', function ($routeParams, $scope, $http) {
+    $http.get("http://www.bdfinanse.pl/bd_site/api/article.php?id="+$routeParams.articleId)
+         .then(function(res){
+            $scope.article = res.data.articles[0];
+          });
+    $scope.articleId = $routeParams.articleId;
+});
